f729f5afa0d1a514ef0f085770b9afb1757af6e7
xinranliang <xinranliang@princeton.edu>
2023-10-09 00:43:52 -0400

    edit

--- domain_classifier/gender.py
+++ domain_classifier/gender.py
@@ -12,11 +12,13 @@ from torch.utils.data import DataLoader, Dataset
 import clip 
 
 DOMAINS = (
-    "male",
-    "female"
+    "woman",
+    "man"
 )
 
 root_path = "/n/fs/xl-diffbia/projects/stable-diffusion/logs/samples"
+
+w_lst = [0.0, 4.0, 6.0, 8.0, 10.0, 12.0, 14.0, 16.0, 18.0, 20.0, 22.0]
 
 
 class TxtImg_Gender(datasets.ImageFolder):
@@ -39,17 +41,17 @@ def clip_predict(batch_size, cfg_w):
     # prepare data
     txtimg_gen_dataset = TxtImg_Gender(root = root_path, transform = preprocess, target_transform = None, cfg_w = cfg_w)
     txtimg_gen_dataloader = DataLoader(txtimg_gen_dataset, batch_size = batch_size, shuffle=False, num_workers=4)
-    domain_text = torch.cat([clip.tokenize(f"a photo of a {domain} person") for domain in DOMAINS]) # num_labels x token_length
+    domain_text = torch.cat([clip.tokenize(f"a photo of a {domain}") for domain in DOMAINS]) # num_labels x token_length
 
-    male_probs, female_probs = [], []
+    pred_labels = []
     for image, class_label in iter(txtimg_gen_dataloader):
         # Calculate features
         with torch.no_grad():
             # discrete domain classification
             logits_per_image, logits_per_text = model(image.to(device), domain_text.to(device))
             probs = logits_per_image.softmax(dim=-1).cpu().numpy() # batch_size x num_domains
-            male_probs.append(probs[:, 0])
-            female_probs.append(probs[:, 1])
+            syn_pred_value = np.argmax(probs, axis=-1)
+            pred_labels.append(syn_pred_value)
 
             # similarity distance metric
             # image_features = model.encode_image(image.to(device))
@@ -58,9 +60,19 @@ def clip_predict(batch_size, cfg_w):
             # text_features /= text_features.norm(dim=-1, keepdim=True)
             # similarity = (image_features @ text_features.T)
     
-    male_probs = np.concatenate(male_probs)
-    female_probs = np.concatenate(female_probs)
+    pred_labels = np.concatenate(pred_labels)
+    num_female = sum(pred_labels == 0)
+    num_male = sum(pred_labels == 1)
+
+    return {"num_male": num_male, "num_female": num_female}
+
+
+def main():
+    for cfg_w in w_lst:
+        return_dict = clip_predict(batch_size=128, cfg_w=cfg_w)
+        print(f"sampling from stable-diffusion-v2 w/ cfg_w = {cfg_w}")
+        print("portion of predicted female: {:03f}".format(return_dict["num_female"] / 1000))
 
 
 if __name__ == "__main__":
-    clip_predict(128, 0.0)
+    main()
--- occupations/utils.py
+++ occupations/utils.py
@@ -1,5 +1,9 @@
 import os
 import subprocess
+import argparse
+
+import torch
+from diffusers import DiffusionPipeline, DPMSolverMultistepScheduler
 
 # 80 train split occupations
 train_list = ['Actor', 'Architect', 'Audiologist', 'Author', 'Baker', 'Barber', 'Blacksmith', 'Bricklayer', 'Bus Driver', 'Butcher', 'Chef', 'Chemist', 'Cleaner', 'Coach', 'Comedian', 'Computer Programmer', 'Construction Worker', 'Consultant', 'Counselor', 'Dancer', 'Dentist', 'Designer', 'Dietitian', 'DJ', 'Doctor', 'Driver', 'Economist', 'Electrician', 'Engineer', 'Entrepreneur', 'Farmer', 'Florist', 'Graphic Designer', 'Hairdresser', 'Historian', 'Journalist', 'Judge', 'Lawyer', 'Librarian', 'Magician', 'Makeup Artist', 'Mathematician', 'Marine Biologist', 'Mechanic', 'Model', 'Musician', 'Nanny', 'Nurse', 'Optician', 'Painter', 'Pastry Chef', 'Pediatrician', 'Photographer', 'Plumber', 'Police Officer', 'Politician', 'Professor', 'Psychologist', 'Real Estate Agent', 'Receptionist', 'Recruiter', 'Researcher', 'Sailor', 'Salesperson', 'Surveyor', 'Singer', 'Social Worker', 'Software Developer', 'Statistician', 'Surgeon', 'Teacher', 'Technician', 'Therapist', 'Tour Guide', 'Translator', 'Vet', 'Videographer', 'Waiter', 'Writer', 'Zoologist']
@@ -7,17 +11,17 @@ train_list = ['Actor', 'Architect', 'Audiologist', 'Author', 'Baker', 'Barber', 
 test_list = ['Accountant', 'Astronaut', 'Biologist', 'Carpenter', 'Civil Engineer', 'Clerk', 'Detective', 'Editor', 'Firefighter', 'Interpreter', 'Manager', 'Nutritionist', 'Paramedic', 'Pharmacist', 'Physicist', 'Pilot', 'Reporter', 'Security Guard', 'Scientist', 'Web Developer']
 
 # guidance values
-w_lst = [1.0, 5.0, 9.0, 13.0, 17.0]
+w_lst = [1.0, 3.0, 5.0, 7.0, 9.0, 11.0, 13.0, 15.0, 17.0, 19.0, 21.0, 23.0]
 
 def get_job_prompt(job_name):
-    return "A single {} in the center.".format(job_name.lower())
+    return "A photo of a single {} standing in the center.".format(job_name.lower())
 
 
 def guidance_sample():
     sys_comm = [
         "python", "scripts/txt2img.py", 
         "--outdir", "./logs", "--ddim_steps", "250", "--ddim_eta", "1.0", "--ckpt", "models/ldm/stable-diffusion-v1/sd-v1-4.ckpt", "--skip_grid",
-        "--n_samples", "5", "--n_iter", "1"
+        "--n_samples", "5", "--n_iter", "20"
     ] 
     for job_name in train_list + test_list:
         input_prompt = get_job_prompt(job_name)
@@ -27,5 +31,40 @@ def guidance_sample():
             subprocess.run(temp_sys_comm)
 
 
+def guidance_sample_loadmodel(batch_size, num_divide, current_divide):
+    prompt_list = [get_job_prompt(job_name) for job_name in train_list] + [get_job_prompt(job_name) for job_name in test_list]
+
+    unit_divide = len(prompt_list) // num_divide
+    prompt_list = prompt_list[unit_divide * current_divide : unit_divide * (current_divide + 1)]
+    
+    repo_id = "stabilityai/stable-diffusion-2-base"
+    pipe = DiffusionPipeline.from_pretrained(repo_id, torch_dtype=torch.float16, revision="fp16")
+    
+    pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
+    pipe = pipe.to("cuda")
+
+    for bs_idx in range(batch_size):
+        for w in w_lst:
+            image_lst = pipe(prompt_list, guidance_scale=w).images
+
+            for curr_idx in range(len(prompt_list)):
+                curr_prompt = prompt_list[curr_idx]
+                curr_job_name = curr_prompt.split(" ")[5]
+                sample_path = os.path.join("./logs/samples", f"guide_w{w - 1.0}", curr_job_name)
+                os.makedirs(sample_path, exist_ok=True)
+                
+                image_lst[curr_idx].save(os.path.join(sample_path, "{:02d}.png".format(bs_idx)))
+
+
 if __name__ == "__main__":
-    guidance_sample()
+    # guidance_sample()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--num-batch", type=int, help="to sample from first or second half of job list")
+    parser.add_argument("--curr-batch", type=int, help="to sample from first or second half of job list")
+    parser.add_argument("--batch-size", type=int, help="number of samples from each prompt")
+
+    opt = parser.parse_args()
+
+    guidance_sample_loadmodel(batch_size = opt.batch_size, num_divide = opt.num_batch, current_divide = opt.curr_batch)
+    
